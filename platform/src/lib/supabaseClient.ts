08a5b5ba2948f9719b7351f54a05b792ea595265
MysticDTA <divinetruthascension@gmail.com>
2025-09-18 21:35:33 +1000

    Ignore local environment files

--- platform/src/lib/supabaseClient.ts
+++ platform/src/lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-// Paste your Project URL here
-const supabaseURL = https://bjyqsebouppdrgjnpblu.supabase.co ;
+// Get values from environment variables
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Paste your anon public key here
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqeXFzZWJvdXBwZHJnam5wYmx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNTc2MjMsImV4cCI6MjA3MzczMzYyM30.KoUAdz-rce4PbhFZdVn8R5HQMiQDzvjnICLwocZHc3A
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Create a single Supabase client for the whole app
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
