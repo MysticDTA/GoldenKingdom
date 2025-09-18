cb6f5d71a8c0054e187dee38cb08e1041bd185e5
MysticDTA <divinetruthascension@gmail.com>
2025-09-19 00:00:04 +1000

    Move app into platform/ and remove old root files

--- pages/sanctuary.js
+++
@@ -1,7 +0,0 @@
-
-# Commit changes
-git add .
-git commit -m "Add sanctuary page"
-
-# Push new branch
-git push origin feature-sanctuary
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header style={{ padding: "1rem", background: "#222", color: "#fff" }}>
-      <h1>Golden Kingdom</h1>
-    </header>
-  );
-}
