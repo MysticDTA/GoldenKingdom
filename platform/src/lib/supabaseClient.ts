afd14787b38ad37dd539403e0c79c373b72ee0ae
MysticDTA <divinetruthascension@gmail.com>
2025-09-18 21:12:34 +1000

    Fix Supabase client syntax error

--- platform/src/lib/supabaseClient.ts
+++ platform/src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Paste your Project URL here
-const supabaseURL = https://bjyqsebouppdrgjnpblu.supabase.co
+const supabaseURL = https://bjyqsebouppdrgjnpblu.supabase.co;
 
 // Paste your anon public key here
 const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqeXFzZWJvdXBwZHJnam5wYmx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNTc2MjMsImV4cCI6MjA3MzczMzYyM30.KoUAdz-rce4PbhFZdVn8R5HQMiQDzvjnICLwocZHc3A
